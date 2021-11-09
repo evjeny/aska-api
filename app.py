dac743dd433922c39e3790d78e8910cd4f32c679
evjeny <gkanafing@gmail.com>
2021-11-09 20:33:37 +0500

    add status

--- app.py
+++ app.py
@@ -3,7 +3,12 @@ from flask import Flask, send_file
 app = Flask(__name__)
 
 
-@app.route("/get_result_viz")
+@app.route("/status", methods=["GET", "POST"])
+def status():
+    return "good!"
+
+
+@app.route("/get_result_viz", methods=["GET", "POST"])
 def get_result_viz():
     return send_file("mock.jpeg", mimetype='image/jpeg')
 
